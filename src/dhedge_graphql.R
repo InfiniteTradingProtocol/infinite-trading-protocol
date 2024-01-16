5fed31689df108c423d351517fe69c64d5f3f2d6
EtherPilled <richard.clare@upr.edu>
2024-01-15 20:04:40 -0400

    GRAPHQL

--- src/dhedge_graphql.R
+++ src/dhedge_graphql.R
@@ -5,7 +5,7 @@
 require(httr)
 require(jsonlite)
 
-dhedge_graphql=function(query="composition"){
+dhedge_graphql=function(query="composition",name=NULL){
   # GraphQL endpoint URL
   graphql_url <- "https://api-v2.dhedge.org/graphql"
   
@@ -17,6 +17,8 @@ dhedge_graphql=function(query="composition"){
           amount
           tokenName
           id
+          rate
+          tokenAddress
         }
         id
         name
@@ -31,8 +33,56 @@ dhedge_graphql=function(query="composition"){
     encode = "json"
   )
   result <- content(response, "text") %>%
-    fromJSON(flatten = TRUE)
-  
+  fromJSON(flatten = TRUE)
+  if (query == "composition") { 
+    fund_names = res$data$allFundsByManager$name
+    n = length(res$data$allFundsByManager$name)
+    for (i in 1:n) { 
+      if (is.null(name)) {
+        print(paste0("Fund name: ",fund_names[i]))
+        print(res$data$allFundsByManager$fundComposition[[i]])
+      }
+      else if (fund_names[i]==name) { 
+        composition = res$data$allFundsByManager$fundComposition[[i]]
+        return(composition)
+      }
+    }
+  }
   return(result)
-  
+}
+coin_amount = function(pool,coins) { 
+  comp = dhedge_graphql(query="composition",name=pool)
+  n = length(coins)
+  tokens_amount = rep(0,n)
+  for (i in 1:n) { 
+    if (any(comp$tokenName == coins[i])) { 
+      index = which(comp$tokenName == coins[i])
+      tokens_amount[i] = as.numeric(comp$amount[index])
+      if (name == "USDCe" || name == "USDT" || name == "USDC") { decimals = 6 }
+      else if (name == "WBTC") { decimals = 8 }
+      else { decimals = 18 }
+      tokens_amount[i] = tokens_amount[i]/(10^decimals)
+    }
+  }
+  return(tokens_amount)
+}
+
+while (1) {
+  #Monitoring deposits
+  name = "Inflation Hedge"
+  deposits = coin_amount(pool=name,coins=c("USDCe","USDC"))
+  if (deposits[1]  >= 10 || deposits[2] >= 10) {
+    discord(msg=paste0("Vault: ",name," USD Balance: ",deposits[1]+deposits[2]),channel="#deposits-alerts")
+  }
+  name = "USD Savings Account"
+  deposits = coin_amount(pool=name,coins=c("USDCe","USDC"))
+  if (deposits[1]  >= 10 || deposits[2] >= 10) {
+    discord(msg=paste0("Vault: ",name," USD Balance: ",deposits[1]+deposits[2]),channel="#deposits-alerts")
+  }
+  name = "Ethereum Savings Account"
+  deposits = coin_amount(pool=name,coins=c("WETH"))
+  if (deposits >= 0.01) {
+    discord(msg=paste0("Vault: ",name," WETH Balance: ",deposits[1]),channel="#deposits-alerts")
+  }
+  Sys.sleep(1)
 }
