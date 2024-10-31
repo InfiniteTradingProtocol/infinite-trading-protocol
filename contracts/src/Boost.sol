5e185ca94a7368bb423fb7cf99436120d6be98dc
marcersst <104516883+marcersst@users.noreply.github.com>
2024-10-30 21:47:17 -0300

    Update Boost.sol
    New functions were added to facilitate the swaptoitp process, and several variables were initialized

--- contracts/src/Boost.sol
+++ contracts/src/Boost.sol
@@ -9,14 +9,34 @@ import "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol"
 import "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {IGauge} from "./interfaces/IGauge.sol"; 
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "forge-std/console.sol"; 
 
-contract BoosterVeloLp is ReentrancyGuard, Ownable {
+
+
+
+interface IVelodromeRouter {
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+}
+
+contract BoosterVeloLp is ReentrancyGuard, Ownable(msg.sender) {
     using SafeERC20 for IERC20;
 
     IERC20 public immutable itpToken;  
     IERC20 public immutable veloToken;  
+    IVelodromeRouter public router;
+    
 
+    uint256 public balanceBelo;  
     uint256 public balanceITP;  
+    uint256 public feePercentage = 1;     
+    uint256 public boostPercentage = 20;
 
     // mappings anidados para manejar  varios lp 
     mapping(address => mapping(address => uint256)) public balanceOf;  
@@ -31,9 +51,13 @@ contract BoosterVeloLp is ReentrancyGuard, Ownable {
     event Withdraw(address indexed user, address indexed lpToken, uint256 amount);
     event ClaimRewards(address indexed user, address indexed lpToken, uint256 reward);
 
-    constructor(address _itpToken, address _veloToken, uint  _balanceITP) {
+     constructor(address _itpToken,address _veloToken, address _router,uint256 _balanceITP) {
+
         itpToken = IERC20(_itpToken);
-        veloToken = IERC20(_veloToken); balanceITP= _balanceITP;
+        veloToken = IERC20(_veloToken);
+        router = IVelodromeRouter(_router); 
+        balanceITP = _balanceITP;
+
     }
 
     /// @notice actualiza las recompensas acumuladas para el usuario y el LP token
@@ -41,19 +65,19 @@ contract BoosterVeloLp is ReentrancyGuard, Ownable {
         IGauge gauge = gauges[lpToken];  
         require(address(gauge) != address(0), "Gauge not registered for this LP token");
 
-        uint256 rewardPerTokenFromGauge = gauge.rewardPerToken();  // Usa el rewardPerToken del Gauge original
-        rewards[_account][lpToken] = earned(_account, lpToken);  // Actualiza las recompensas pendientes para este LP
+        uint256 rewardPerTokenFromGauge = gauge.rewardPerToken(); 
+        rewards[_account][lpToken] = earned(_account, lpToken);  
         userRewardPerTokenPaid[_account][lpToken] = rewardPerTokenFromGauge;  
     }
 
     /// @notice calcula cuantas recompensas ha ganado el usuario hasta el momento
     /// @dev utilizo el valor de `rewardPerToken()` del Gauge original de Velodrome.
     function earned(address _account, address lpToken) public view returns (uint256) {
-        IGauge gauge = gauges[lpToken];  // obtener el Gauge correspondiente al LP token
+        IGauge gauge = gauges[lpToken];  
         require(address(gauge) != address(0), "Gauge not registered for this LP token");
 
         uint256 rewardPerTokenFromGauge = gauge.rewardPerToken(); 
-        uint256 userBalance = balanceOf[_account][lpToken];  // Balance del usuario en tokens LP
+        uint256 userBalance = balanceOf[_account][lpToken];  
 
         // calculo de las recompensas acumuladas basado en el  balance del usuario 
         return (userBalance * (rewardPerTokenFromGauge - userRewardPerTokenPaid[_account][lpToken])) / 1e18 + rewards[_account][lpToken];
@@ -67,11 +91,11 @@ contract BoosterVeloLp is ReentrancyGuard, Ownable {
         require(address(gauge) != address(0), "Gauge not registered for this LP token");
 
 
+        _updateRewards(msg.sender, lpToken);  // actualiza las recompensas antes del cambio de balance
         // obtener el Gauge correspondiente para este LP token
         IERC20(lpToken).safeTransferFrom(msg.sender, address(this), _amount);
 
         // transferir los LP tokens del usuario a tu contrato booster
-        _updateRewards(msg.sender, lpToken);  // Actualiza las recompensas antes del cambio de balance
 
         IERC20(lpToken).safeIncreaseAllowance(address(gauge), _amount);
 
@@ -84,7 +108,7 @@ contract BoosterVeloLp is ReentrancyGuard, Ownable {
         emit Deposit(msg.sender, lpToken, _amount);
     }
 
-    /// @notice Permite a los usuarios retirar sus tokens LP y reclamar sus recompensas.
+    /// @notice permite a los usuarios retirar sus tokens LP y reclamar sus recompensas
 
     function withdraw(uint256 _amount, address lpToken) external nonReentrant {
         require(_amount > 0, "Cannot withdraw 0 tokens");
@@ -98,20 +122,52 @@ contract BoosterVeloLp is ReentrancyGuard, Ownable {
 
         balanceOf[msg.sender][lpToken] -= _amount;  // actualiza el balance del usuario para este LP
 
-        // Se resta el %0.1 de la cantidad depositada 
-            uint256 fee = (_amount * 1) / 1000; 
- 
-            uint256 amount = _amount - fee;
+        uint256 fee = (_amount * feePercentage) / 1000; 
+        uint256 amount = _amount - fee;
 
+        gauge.getReward(address(this));
         gauge.withdraw(amount);
-        IERC20(lpToken).safeTransfer(msg.sender,amount);  // regreso los lp al usuario
+        IERC20(lpToken).safeTransfer(msg.sender,amount);
 
-        lpFee[lpToken]+=fee;
+        uint256 _rewards = earned(msg.sender, lpToken);
+        if(_rewards>0){
+            rewards[msg.sender][lpToken]=0;
 
-        ///aca va a ir logica para manejar el %20 mas en $ITP al usuario
+            uint itpReward =swapVeloToItp(_rewards);
+            uint256 extraReward = (itpReward * boostPercentage) / 100;
+            balanceITP -= extraReward;
+            itpToken.safeTransfer(msg.sender, itpReward + extraReward);
+            lpFee[lpToken]+=fee;
+        }
+
+        
        
             }
 
+        function swapVeloToItp(uint256 amountIn) internal returns (uint256) {
+
+        veloToken.forceApprove(address(router), amountIn);
+
+        address [] memory path = new address[](2);    
+        path[0] = address(veloToken); 
+        path[1] = address(itpToken);   
+
+        
+        uint256 amountOutMin = 0;
+        uint256 deadline = block.timestamp + 120;
+
+        // realiza el swap usando el router de Velodrome
+        uint256[] memory amountsOut = router.swapExactTokensForTokens(
+            amountIn,
+            amountOutMin,
+            path,
+            address(this),
+            deadline
+        );
+
+        // Devuelve la cantidad final recibida de itpToken
+        return amountsOut[amountsOut.length - 1];
+    }
 
     /**
      * @notice permite al owner reclamar las recompensas generadas en VELO para un LP token 
@@ -149,6 +205,7 @@ contract BoosterVeloLp is ReentrancyGuard, Ownable {
 
     }
 
+
     function withdrawITP(uint256 _amount, address _recipient) external onlyOwner nonReentrant {
         require(_amount > 0, "Cannot withdraw 0 tokens");
         require(_amount <= balanceITP, "Insufficient ITP balance in contract");
@@ -165,7 +222,7 @@ contract BoosterVeloLp is ReentrancyGuard, Ownable {
         }
 
         /**
-         * @notice permite inicializar los gauges
+         * @notice permite inicializar multiples gauges en una sola llamada
          */
 
     function initializeGauges(address[] calldata lpTokens, IGauge[] calldata _gauges) external onlyOwner {
@@ -187,4 +244,35 @@ contract BoosterVeloLp is ReentrancyGuard, Ownable {
             gauges[lpToken] = gauge;
         }
 
+
+    function BalanceOfLp (address user, address lpToken ) external view returns(uint256){
+        return balanceOf[user][lpToken];
+    }
+
+    
+    function obtenerrecompensas(address lpToken) external {
+        IGauge gauge = gauges[lpToken];
+        require(address(gauge) != address(0), "Gauge not registered for this LP token");
+
+        gauge.getReward(address(this));
+        
+    }
+
+
+
+    function setRouter(address _router) external onlyOwner {
+        require(_router != address(0), "Invalid router address");
+        router = IVelodromeRouter(_router);
+    }
+  
+    function setFeePercentage(uint256 newFeePercentage) external onlyOwner {
+        feePercentage = newFeePercentage;
+    }
+
+    
+    function setBoostPercentage(uint256 newBoostPercentage) external onlyOwner {
+        boostPercentage = newBoostPercentage;
+    }
+
 }
+
