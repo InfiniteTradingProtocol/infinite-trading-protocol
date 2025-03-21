700207c6627ebe34caa1f2fb0db53425aa95cf8a
Marcelo Rosselot <104516883+marcersst@users.noreply.github.com>
2025-03-21 09:28:33 -0300

    Update InfiniteBoost.sol

--- contracts/src/InfiniteBoost.sol
+++ contracts/src/InfiniteBoost.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+* @title InfiniteBoost
+ * @dev Sistema de staking con boosting de recompensas para LP tokens
+ * @version 1.0.0
+ * @notice Este contrato permite a los usuarios hacer staking de sus LP tokens y recibir recompensas con un boost adicional
+ * @custom:github https://github.com/marcersst
+
+
+
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
